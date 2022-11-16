5dc3da4f6f6f50317823077419ecd8058b476b6d
bhavyasree627 <99475745+bhavyasree627@users.noreply.github.com>
2022-11-16 07:33:46 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,13 @@
+function myFunction(){
+document.getElementById("demo").innerHTML="h1 I am";
+}
+function hello(){
+alert("hello world");
+}
+function text(){
+prompt("enter name");
+}
+document.write("<h2>i am h2</h2>");
+var nam=prompt("whats your name");
+document.write(nam);
+document.write("<h3>"+" space "+</h3>)
